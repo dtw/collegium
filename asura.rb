62b9f6938dead839807a8bdc433e155cf8f83f4d
Phil T <dibblethewrecker@gmail.com>
2016-08-01 13:59:10 +0100

    v0.2

--- asura.rb
+++ asura.rb
@@ -3,10 +3,29 @@
 a = ('a'..'z').to_a
 v = %w[a e i o u]
 c = a - v
+r = []
 
-name = c[rand(c.length)].upcase
-name += v[rand(v.length)] 
-name += c[rand(c.length)] * 2
-name += v[rand(v.length)] 
+name = r[rand(r.length)]
+			puts name
+			
+def generate_name name_crib
+	name_crib.each do |item|
+		puts item
+		if name.length == 0 then
+			name = item[rand(item.length)]
+			puts name
+		elsif item = v or item = c
+			name += r[0]
+			puts name
+		else
+			r[0] = item[rand(item.length)]
+			name += r[0]
+			puts name
+		end
+	end
+	return name
+end
 
-puts 'Name: ' + name
+name_crib = 
+
+puts 'Name: ' + generate_name([c,v,c,r])
